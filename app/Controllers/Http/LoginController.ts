abafc092de5a81e8d04b16199eff774c030444e8
Mauro Oviedo <mauro.oviedo@protonmail.com>
2021-02-01 21:25:51 -0500

    Implemented login form logic based on tests.

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -1,6 +1,27 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import LoginValidator, { messages } from 'App/Validators/LoginValidator'
+import Route from '@ioc:Adonis/Core/Route'
+
 export default class LoginController {
   public async index({ view }: HttpContextContract) {
     return view.render('login', { title: 'Login' })
+  }
+
+  public async submit({ auth, request, response, session }: HttpContextContract) {
+    try {
+      const { username, password, rememberMe = false } = await request.validate(LoginValidator)
+      await auth.attempt(username, password, rememberMe)
+
+      response.redirect(Route.makeUrl('home') || '/')
+    } catch (error) {
+      if (error.code === 'E_INVALID_AUTH_UID' || error.code === 'E_INVALID_AUTH_PASSWORD') {
+        // unable to find user using account
+        session.flash({
+          invalid: messages.invalid,
+        })
+      }
+
+      throw error
+    }
   }
 }
