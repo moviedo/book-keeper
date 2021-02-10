c88ed5eb3e88435f3e5f74cbdfa865996ba445a5
Mauro Oviedo <mauro.oviedo@protonmail.com>
2021-02-10 14:28:38 -0500

    Initial commit for vue integration.

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -1,7 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Application from '@ioc:Adonis/Core/Application'
 
 export default class DashboardController {
   public async index({ view }: HttpContextContract) {
-    return view.render('dashboard', { title: 'Dashboard' })
+    return view.render('dashboard', { title: 'Dashboard', application: Application })
   }
 }
