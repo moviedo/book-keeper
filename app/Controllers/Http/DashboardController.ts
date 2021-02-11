edcfe7ca85011ae5da9e214015b3796100be2643
Mauro Oviedo <mauro.oviedo@protonmail.com>
2021-02-11 11:51:10 -0500

    Resolved issue with using app global.

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -1,8 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Application from '@ioc:Adonis/Core/Application'
 
 export default class DashboardController {
   public async index({ view }: HttpContextContract) {
-    return view.render('dashboard', { title: 'Dashboard', application: Application })
+    return view.render('dashboard', { title: 'Dashboard' })
   }
 }
